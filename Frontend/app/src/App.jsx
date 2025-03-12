09399219a66539d32a5538c62e209ca7f1899ece
sab-eeh <usabeeh72@gmail.com>
2025-03-13 04:11:15 +0500

    remove components from App.jsx

--- Frontend/app/src/App.jsx
+++ Frontend/app/src/App.jsx
@@ -1,15 +1,12 @@
 import Header from "./layouts/Header";
 import HomePage from "./pages/HomePage";
-import WorkExperience from "./components/WorkExperience";
-import HeadlineSection from "./components/HeadlineSection";
+
 
 function App() {
   return (
     <>
       <Header />
       <HomePage />
-      <HeadlineSection />
-      <WorkExperience />
     </>
   );
 }
